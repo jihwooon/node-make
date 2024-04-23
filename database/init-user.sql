ba1b512b838ebe40ea75fef92ef22ed528fbdc26
jihwooon <jihwooon@gmail.com>
2024-04-23 15:13:03 +0900

    init sql 수정

--- database/init-user.sql
+++ database/init-user.sql
@@ -4,3 +4,6 @@ CREATE USER IF NOT EXISTS 'prgms'@'%' IDENTIFIED BY 'prgms';
 GRANT ALL PRIVILEGES ON prgms_notes.* TO 'prgms'@'localhost';
 GRANT ALL PRIVILEGES ON prgms_notes.* TO 'prgms'@'%';
 FLUSH PRIVILEGES;
+
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporterpassword' WITH MAX_USER_CONNECTIONS 3;
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
